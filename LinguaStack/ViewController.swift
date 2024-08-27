b1d8a6d21001812d35f3b3db135ddab402075022
Philipp Lazarev <philipp.lazareff@gmail.com>
2024-08-27 22:31:10 +0300

    Initial commit for develop branch

---
+++ LinguaStack/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  LinguaStack
+//
+//  Created by Philipp Lazarev on 27.08.2024.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
