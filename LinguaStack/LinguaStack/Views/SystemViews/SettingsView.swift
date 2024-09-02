854f81248b2a792de2709d0f33d18f7b4ff863fd
Philipp Lazarev <philipp.lazareff@gmail.com>
2024-09-02 23:24:35 +0300

    Added Russian localization

--- LinguaStack/LinguaStack/Views/SystemViews/SettingsView.swift
+++ LinguaStack/LinguaStack/Views/SystemViews/SettingsView.swift
@@ -10,7 +10,15 @@ import SwiftUI
 
 struct SettingsView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Form {
+            Section("App Interface") {
+                
+            }
+            
+            Section("Notifications") {
+                
+            }
+        }
     }
 }
 
